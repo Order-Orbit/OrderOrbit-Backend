5e1ae2ba21849daf283360c4bd60f86a276b9e9e
abumulla <abu.works.at.this@gmail.com>
2024-04-22 16:41:04 +0530

    project setup

---
+++ src/main/java/com/orderorbit/orderorbit/OrderorbitApplication.java
@@ -0,0 +1,13 @@
+package com.orderorbit.orderorbit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderorbitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderorbitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orderorbit/orderorbit/OrderorbitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderorbit.orderorbit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderorbitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
