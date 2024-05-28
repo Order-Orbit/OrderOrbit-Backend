111a27e0b4cf0e652fd3aacc64fcbf8b9223e20d
abumulla <abu.works.at.this@gmail.com>
2024-05-28 18:09:14 +0530

    added validations for corner test cases

--- src/main/java/com/orderorbit/orderorbit/service/impl/RestaurantServiceImpl.java
+++ src/main/java/com/orderorbit/orderorbit/service/impl/RestaurantServiceImpl.java
@@ -27,7 +27,7 @@ import com.orderorbit.orderorbit.utility.OrderStatus;
 import com.orderorbit.orderorbit.utility.Role;
 
 @Service
-public class RestaurantServiceImpl implements RestaurantService{
+public class RestaurantServiceImpl implements RestaurantService {
 
     @Autowired
     JwtTokenUtil tokenObj;
@@ -50,177 +50,193 @@ public class RestaurantServiceImpl implements RestaurantService{
     @Autowired
     AwsS3Util awsS3UtilObj;
 
-    public String hashPassword(String password){
+    public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt(10));
     }
 
     @Override
     public Menu addMenuItem(String token, String mitemName, long mitemPrice, MultipartFile img) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
                 String reqEmail = tokenObj.getEmailFromToken(token);
                 Restaurant res = restaurantRepository.findByrEmail(reqEmail).get();
                 Menu menu = new Menu();
                 menu.setRId(res.getRId());
                 menu.setMItemName(mitemName);
                 menu.setMItemPrice(mitemPrice);
-                try {
-                    // AWS S3 code
-                    menu.setMItemPhoto(awsS3UtilObj.uploadFileToS3(img));
-                } catch (Exception e) {
-                    System.out.println(e.getStackTrace());
+                if (img == null) {
+                    menu.setMItemPhoto(null);
+                } else {
+                    try {
+                        // AWS S3 code
+                        menu.setMItemPhoto(awsS3UtilObj.uploadFileToS3(img));
+                    } catch (Exception e) {
+                        System.out.println(e.getStackTrace());
+                    }
                 }
                 return menuRepository.save(menu);
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
         }
     }
 
     @Override
     public List<Menu> getMenus(String token) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
                 String reqEmail = tokenObj.getEmailFromToken(token);
                 Restaurant res = restaurantRepository.findByrEmail(reqEmail).get();
                 return menuRepository.findAllByrId(res.getRId()).get();
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
         }
     }
 
     @Override
     public Menu updateMenuItem(UUID mItemId, MultipartFile img, String token, String mitemName, long mitemPrice) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
-                Menu newMenu = menuRepository.findById(mItemId).get();
-                newMenu.setMItemName(mitemName);
-                newMenu.setMItemPrice(mitemPrice);
-                try {
-                    String oldPhotoUrl = newMenu.getMItemPhoto();
-                    String newPhotoUrl = awsS3UtilObj.uploadFileToS3(img);
-                    if(newPhotoUrl.equals(null)){
-                        newMenu.setMItemPhoto(oldPhotoUrl);
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
+                if (menuRepository.existsById(mItemId)) {
+                    Menu newMenu = menuRepository.findById(mItemId).get();
+                    newMenu.setMItemName(mitemName);
+                    newMenu.setMItemPrice(mitemPrice);
+                    if (img == null) {
+                        newMenu.setMItemPhoto(newMenu.getMItemPhoto());
+                    } else {
+                        try {
+                            String oldPhotoUrl = newMenu.getMItemPhoto();
+                            String newPhotoUrl = awsS3UtilObj.uploadFileToS3(img);
+                            if (newPhotoUrl.equals(null)) {
+                                newMenu.setMItemPhoto(oldPhotoUrl);
+                            } else {
+                                newMenu.setMItemPhoto(newPhotoUrl);
+                                // if(!(oldPhotoUrl.equals(null))){
+                                // System.out.println(awsS3UtilObj.deleteFileFromS3(oldPhotoUrl));
+                                // }
+                                if (!(oldPhotoUrl == null)) {
+                                    System.out.println(awsS3UtilObj.deleteFileFromS3(oldPhotoUrl));
+                                }
+                            }
+
+                        } catch (Exception e) {
+                            System.out.println(e.getMessage());
+                        }
                     }
-                    else{
-                        newMenu.setMItemPhoto(newPhotoUrl);
-                        awsS3UtilObj.deleteFileFromS3(oldPhotoUrl);
-                    }
-                    
-                } catch (Exception e) {
-                    System.out.println(e.getStackTrace());
+                    return menuRepository.save(newMenu);
+                } else {
+                    throw new ResourceNotFoundException("Menu Item ID", "mItemId", mItemId);
                 }
-                return menuRepository.save(newMenu);
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
-        }   
+        }
     }
 
     @Override
     public String deleteMenuItem(UUID mItemId, String token) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
-                if (menuRepository.existsById(mItemId)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
+                if (menuRepository.existsById(mItemId)) {
+                    Menu menu = menuRepository.findById(mItemId).get();
+                    if (!(menu.getMItemPhoto() == null)) {
+                        try {
+                            System.out.println(awsS3UtilObj.deleteFileFromS3(menu.getMItemPhoto()));
+                        } catch (Exception e) {
+                            System.err.println(e.getMessage());
+                        }
+                    }
                     menuRepository.deleteById(mItemId);
-                    return String.format("Menu Item with Id: %s deleted successfully!",mItemId);
-                }
-                else{
+                    return String.format("Menu Item with Id: %s deleted successfully!", mItemId);
+                } else {
                     throw new ResourceNotFoundException("ID", "mItemId", mItemId);
                 }
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
-        }   
+        }
     }
 
     @Override
     public List<Orders> allOrdersAtRestaurantDashboard(String token) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
                 String rEmail = tokenObj.getEmailFromToken(token);
                 Restaurant rest = restaurantRepository.findByrEmail(rEmail).get();
                 return ordersRepository.findAllByrId(rest.getRId()).get();
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
-        }   
+        }
     }
 
     @Override
     public String updateOStatusToCompl(UUID oId, String token) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
-                Orders order = ordersRepository.findById(oId).get();
-                Customer cust = customerRepository.findById(order.getCId()).get();
-                String cEmail = cust.getCEmail();
-                String orderItems = order.getOItems();
-                String forderItems = String.join(" : ", String.join("\n", orderItems.split("-")).split("@"));
-                SimpleMailMessage message = new SimpleMailMessage();
-                message.setTo(cEmail);
-                message.setSubject(String.format("OrderOrbit: Your Order is ready!!"));
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
+                if (ordersRepository.existsById(oId)) {
+                    Orders order = ordersRepository.findById(oId).get();
+                    Customer cust = customerRepository.findById(order.getCId()).get();
+                    String cEmail = cust.getCEmail();
+                    String orderItems = order.getOItems();
+                    String forderItems = String.join(" : ", String.join("\n", orderItems.split("-")).split("@"));
+                    SimpleMailMessage message = new SimpleMailMessage();
+                    message.setTo(cEmail);
+                    message.setSubject(String.format("OrderOrbit: Your Order is ready!!"));
 
-                message.setText(String.format("Hi %s,\nWe know that you are waiting for a long, but no more waiting now!\n\nYour order at %s with order Id: %s is ready now.\n\nYour order details:\n%s\nTotal payment done: %s\n\nHope you enjoy the food.\nThank you for choosing us.\n\nWarm regards,\n%s",cust.getCName(),order.getRName(),order.getOId().toString(),forderItems,String.valueOf(order.getOCost()),order.getRName()));
-                try {
-                    javaMailSender.send(message);
-                    order.setOStatus(OrderStatus.COMPLETED);
-                    ordersRepository.save(order);
-                    return String.format("OrderStatus updated and mail notification sent to Customer with Id: %s",cust.getCId());
-                } catch (Exception e) {
-                    return e.getStackTrace().toString();
+                    message.setText(String.format(
+                            "Hi %s,\nWe know that you are waiting for a long, but no more waiting now!\n\nYour order at %s with order Id: %s is ready now.\n\nYour order details:\n%s\nTotal payment done: %s\n\nHope you enjoy the food.\nThank you for choosing us.\n\nWarm regards,\n%s",
+                            cust.getCName(), order.getRName(), order.getOId().toString(), forderItems,
+                            String.valueOf(order.getOCost()), order.getRName()));
+                    try {
+                        javaMailSender.send(message);
+                        order.setOStatus(OrderStatus.COMPLETED);
+                        ordersRepository.save(order);
+                        return String.format("OrderStatus updated and mail notification sent to Customer with Id: %s",
+                                cust.getCId());
+                    } catch (Exception e) {
+                        return e.getStackTrace().toString();
+                    }
+                } else {
+                    throw new ResourceNotFoundException("Order ID", "oId", oId);
                 }
-
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
         }
     }
 
     @Override
     public Restaurant getRestaurantProfile(String token) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
                 String rEmail = tokenObj.getEmailFromToken(token);
                 return restaurantRepository.findByrEmail(rEmail).get();
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
         }
     }
 
     @Override
     public Restaurant updateRestaurantProfile(String token, Restaurant rest) {
-        if(tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())){
-            if (tokenObj.verifyToken(token)){
+        if (tokenObj.getRoleFromToken(token).equals(Role.RESTAURANT.toString())) {
+            if (tokenObj.verifyToken(token)) {
                 String rEmail = tokenObj.getEmailFromToken(token);
                 Restaurant res = restaurantRepository.findByrEmail(rEmail).get();
                 res.setRName(rest.getRName());
@@ -228,14 +244,12 @@ public class RestaurantServiceImpl implements RestaurantService{
                 res.setRAddress(rest.getRAddress());
                 res.setRPassword(hashPassword(rest.getRPassword()));
                 return restaurantRepository.save(res);
-            }
-            else{
+            } else {
                 throw new AuthorizationException("Invalid token, Login again");
             }
-        }
-        else{
+        } else {
             throw new AuthorizationException("Access available only for Restaurants");
         }
     }
-    
+
 }
--- todo.md
+++ todo.md
@@ -1,6 +1,6 @@
 ## TO-DO: 
 
-1. Payment gateway for placing order
-2. Otp-based registeration, Otp-based forgot password.
+1. Otp-based registeration, Otp-based forgot password.
+2. Payment gateway for placing order
 
 3. Front-end: polling to fetch orders to dashboard periodically without need of refresh
